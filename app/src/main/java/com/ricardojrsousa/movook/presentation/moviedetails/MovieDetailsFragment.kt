9dae8525cb65651a35d95581d8142fd21ae21c3f
Ricardo sousa <ricardojrsousa@outlook.pt>
2021-02-11 13:42:11 +0000

    Added movie trailer and fixed splash screen logo

--- app/src/main/java/com/ricardojrsousa/movook/presentation/moviedetails/MovieDetailsFragment.kt
+++ app/src/main/java/com/ricardojrsousa/movook/presentation/moviedetails/MovieDetailsFragment.kt
@@ -2,6 +2,7 @@ package com.ricardojrsousa.movook.presentation.moviedetails
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.MediaController
@@ -29,7 +30,7 @@ import kotlinx.android.synthetic.main.fragment_movie_details.view.*
 
 
 @AndroidEntryPoint
-class MovieDetailsFragment : BaseFragment<MovieDetailsViewModel>(R.layout.fragment_movie_details), YouTubePlayerListener {
+class MovieDetailsFragment : BaseFragment<MovieDetailsViewModel>(R.layout.fragment_movie_details) {
 
     override val viewModel: MovieDetailsViewModel by viewModels()
     private lateinit var movieId: String
@@ -51,6 +52,11 @@ class MovieDetailsFragment : BaseFragment<MovieDetailsViewModel>(R.layout.fragme
 
         setupRecyclerView(view, similarMoviesAdapter, relatedBooksAdapter, castAdapter)
         observeViewModel(similarMoviesAdapter, relatedBooksAdapter, castAdapter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        movie_trailer.release()
     }
 
     private fun setupRecyclerView(
@@ -128,14 +134,15 @@ class MovieDetailsFragment : BaseFragment<MovieDetailsViewModel>(R.layout.fragme
     }
 
     private fun setupVideoView(movieTrailerUrl: String?) {
-        movieTrailerUrl?.let { url ->
+        if (!movieTrailerUrl.isNullOrBlank()) {
             lifecycle.addObserver(movie_trailer)
             movie_trailer.getYouTubePlayerWhenReady(object : YouTubePlayerCallback {
                 override fun onYouTubePlayer(youTubePlayer: YouTubePlayer) {
-                    youTubePlayer.cueVideo(url, 0f)
+                    youTubePlayer.cueVideo(movieTrailerUrl, 0f)
                 }
             })
-            movie_trailer.addYouTubePlayerListener(this)
+        } else {
+            movie_trailer.visibility = View.GONE
         }
     }
 
@@ -171,47 +178,5 @@ class MovieDetailsFragment : BaseFragment<MovieDetailsViewModel>(R.layout.fragme
             related_books_label.visibility = View.VISIBLE
             related_books_list.visibility = View.VISIBLE
         }
-    }
-
-    override fun onApiChange(youTubePlayer: YouTubePlayer) {
-        //Do nothing
-    }
-
-    override fun onCurrentSecond(youTubePlayer: YouTubePlayer, second: Float) {
-        //Do nothing
-    }
-
-    override fun onError(youTubePlayer: YouTubePlayer, error: PlayerConstants.PlayerError) {
-        movie_trailer.visibility = View.INVISIBLE
-        no_trailer_textview.visibility = View.VISIBLE
-
-    }
-
-    override fun onPlaybackQualityChange(youTubePlayer: YouTubePlayer, playbackQuality: PlayerConstants.PlaybackQuality) {
-        //Do nothing
-    }
-
-    override fun onPlaybackRateChange(youTubePlayer: YouTubePlayer, playbackRate: PlayerConstants.PlaybackRate) {
-        //Do nothing
-    }
-
-    override fun onReady(youTubePlayer: YouTubePlayer) {
-        //Do nothing
-    }
-
-    override fun onStateChange(youTubePlayer: YouTubePlayer, state: PlayerConstants.PlayerState) {
-        //Do nothing
-    }
-
-    override fun onVideoDuration(youTubePlayer: YouTubePlayer, duration: Float) {
-        //Do nothing
-    }
-
-    override fun onVideoId(youTubePlayer: YouTubePlayer, videoId: String) {
-        //Do nothing
-    }
-
-    override fun onVideoLoadedFraction(youTubePlayer: YouTubePlayer, loadedFraction: Float) {
-        //Do nothing
     }
 }
